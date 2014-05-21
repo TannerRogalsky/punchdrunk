bc489076f98a6f95131a79ce25b3ccc18386a72a
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-05-21 07:21:53 -0400

    hello world intro project

--- lua/conf.lua
+++ lua/conf.lua
@@ -1,2 +1,17 @@
 function love.conf(t)
+  t.title = "Hello, Punchdrunk"
+  t.author = "Tanner Rogalsky"
+  t.identity = "punchdrunk"
+  t.window.width = 900
+  t.window.height = 300
+  t.modules.joystick = true
+  t.modules.audio = true
+  t.modules.keyboard = true
+  t.modules.event = true
+  t.modules.image = true
+  t.modules.graphics = true
+  t.modules.timer = true
+  t.modules.mouse = true
+  t.modules.sound = true
+  t.modules.physics = true
 end
