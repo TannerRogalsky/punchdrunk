36175d567c78f6351a597345c6844fe504954f36
Tanner Rogalsky <tanner@tannerrogalsky.com>
2014-06-10 22:02:44 -0400

    test filled polygons

--- examples/webgl/main.lua
+++ examples/webgl/main.lua
@@ -6,12 +6,17 @@ end
 
 function love.update(dt)
   index = index + 1
+  love.graphics.setBackgroundColor(index % 255, 0, 255 - index % 255)
 end
 
 function love.draw()
   local g = love.graphics
+  g.setColor(255, 255, 255)
+  g.polygon("fill", 100, 100, 200, 200, 100, 200)
 
-  g.setBackgroundColor(index % 255, 0, 0)
+  g.setColor(0, 255, 0, 100)
+  g.polygon("fill", 300, 300, 200, 200, 300, 200)
+
   -- g.circle("fill", 50, 50 + 25, 25)
 
   -- g.setColor(100, 100, 255)
